4bcefeda479a6792f445063c59f47bec97963d3d
hari babu <haribabu.c535@gmail.com>
2023-11-27 03:06:24 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
 
-function App() {
+const App = () => {
+  const navigate = useNavigate();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+
+      <button onClick={() => navigate("about")}>go to some page</button>
     </div>
   );
-}
+};
 
 export default App;
