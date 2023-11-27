81a7ce55c30c4e18869aad44958145ac63246234
hari babu <haribabu.c535@gmail.com>
2023-11-27 20:31:30 +0530

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
-import ContextAcceptor from "./ContextAcceptor";
+import React, { useReducer } from "react";
+
+const defaultState = {
+  count: 0,
+  isLoading: true,
+};
+const reducer = (state, action) => {
+  if (action.type === "increase_count") {
+    return { ...state, count: state.count + 1, isLoading: !state.isLoading };
+  }
+  throw new Error("eren yeager");
+};
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, defaultState);
+
   return (
     <div>
-      <ContextAcceptor />
+      {state.count}{" "}
+      <button onClick={() => dispatch({ type: "increase_count" })}>
+        increase count
+      </button>
+      {state.isLoading && <h2>heading 2</h2>}
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import GloballyContext from "./GlobalContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-  <GloballyContext>
-    <App />
-  </GloballyContext>
-);
+root.render(<App />);
